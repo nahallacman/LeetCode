b3131b2f5fbdf8238097a9761c2c91eaa56e5d17
Cal <nahallacman@gmail.com>
2025-09-04 20:34:21 -0700

    Worked myself into a corner, needs a complete redo.

--- problem_008/solution.py
+++ problem_008/solution.py
@@ -9,6 +9,10 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 # Now you can import your wrapper
 from test_runner.wrapper import run_tests
 
+MAX_32_BIT_INT = (2**31) -1
+MAX_32_BIT_INT_NEGATIVE = -2**(31)
+
+# TODO: this needs a rework to match THEIR description of the algorithm instead of doing it how I think it should be done. Only leading whitespace is removed, etc.
 class Solution:
     # We should just have a few simple rules:
     # If you find a character, stop scanning
@@ -18,18 +22,33 @@ class Solution:
     def myAtoi(self, s: str) -> int:
         retval = 0
         numberString = ""
+        signFound = False
         for str in s:
             if (str >= "a" and str <= "z") or (str >= "A" and str <= "Z"):
                 # Stop scanning
                 break
             elif (str == " " or str == "\t" or str == "\n"):
                 # Do nothing and move on to the next character
-                continue
+                if(numberString == "") or not signFound:
+                    continue
+                # If there are already characters in the number string, we have reached the end of the number and stop scanning
+                else:
+                    break
             elif (str == "-"):
+                # If we find more than one sign, stop processing
+                if signFound == True:
+                    break
+                signFound = True
+                # If we have a value in the number string (aka not at the beginning of the string) stop processing
                 if numberString == "":
                     numberString += str
                 else:
                     break
+            elif (str == "+"):
+                # If we find more than one sign, stop processing
+                if(signFound == True):
+                    break
+                signFound = True
             elif (str >= "0" and str <= "9"):
                 numberString += str
             else:
@@ -41,6 +60,12 @@ class Solution:
             retval = int(numberString)
         except Exception as e:
             print(f"Exception e = {e}")
+
+        # Cover max and min 32 bit signed integer cases
+        if(retval > MAX_32_BIT_INT):
+            retval = MAX_32_BIT_INT
+        elif(retval < MAX_32_BIT_INT_NEGATIVE):
+            retval = MAX_32_BIT_INT_NEGATIVE
 
         print(f"converted value = {retval}")
         return retval
