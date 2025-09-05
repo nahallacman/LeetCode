e58680c3379fba64ad08e0d27042f7b0e82e5ebd
Cal <nahallacman@gmail.com>
2025-09-05 15:54:44 -0700

    Brute force solution with poor memory and time optimization.

--- problem_007/solution.py
+++ problem_007/solution.py
@@ -9,10 +9,49 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 # Now you can import your wrapper
 from test_runner.wrapper import run_tests
 
+MAX_32_BIT_INT = (2**31) -1
+MAX_32_BIT_INT_NEGATIVE = -2**(31)
+
 class Solution:
     def reverse(self, x: int) -> int:
-        
-        return 0
+        intAsStr = str(x)
+        print(intAsStr)
+        reverseIntAsStr = []
+        i = len(intAsStr)
+        sign = None
+
+        for x in range(i):
+            reverseIntAsStr.append("")
+
+        for x in intAsStr:
+            i = i - 1
+            if(x == "-" or x == "+"):
+                # sign symbol found, save it off and remove it from the reversal process
+                sign = x
+                reverseIntAsStr[0] = x
+                i = i + 1
+            elif(x >= "0" and x <= "9"):
+                # this is a number, so add it to the string
+                reverseIntAsStr[i] = x
+            else:
+                # this is not a number, skip
+                continue
+
+        print(reverseIntAsStr)
+
+        finalStr = "".join(reverseIntAsStr)
+        print(finalStr)
+
+        retval = int(finalStr)
+
+        # Ensure that the integer we found is within 32 bit signed values (only doing it this way because of the strange way python treats integers. If this was C I would do it very differently.)
+        # Cover max and min 32 bit signed integer cases
+        if(retval > MAX_32_BIT_INT):
+            retval = 0
+        elif(retval < MAX_32_BIT_INT_NEGATIVE):
+            retval = 0
+
+        return retval
 
 
 if __name__ == "__main__":
