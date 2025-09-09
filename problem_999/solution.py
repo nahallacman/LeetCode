884f4530ec3e583997957c3c7cb16c35a01a091e
Cal Barkman <callahan.barkman@gmail.com>
2025-09-08 20:03:28 -0700

    Try to shorten the solution by removing some of the intermdiates.

--- problem_999/solution.py
+++ problem_999/solution.py
@@ -13,7 +13,7 @@ from collections import defaultdict
 
 class Solution:
     # You’ll be given a list where the value at each index represents the size of the group that person belongs to. Your task is to group people into lists of the correct size.
-    def groupBasedOnID(self, group_size_list: str) -> list[list]:
+    def __groupBasedOnID(self, group_size_list: str) -> list[list]:
         retval = []
         group_size_dict = defaultdict(list)
         index = 0
@@ -26,16 +26,44 @@ class Solution:
             except:
                 pass
 
-        # Sort the dictionary by key size
-        sorted_groups = sorted(group_size_dict.items())
-
-        for group, list_index in sorted_groups:
+        # Sort the dictionary by key size, iterate over them getting the group size and the index of the people of that size
+        for group, list_index in sorted(group_size_dict.items()):
+            # While there are items in the list of the current group
             while list_index:
+                # Create a new list that has up to the group size number of people in it.
                 iter = 0
                 temp_list = []
+                # Iterate through the current group and add people to a temporary list while removing people from the possible group.
                 while iter < group:
                     temp_list.append(list_index.pop(0))
                     iter = iter + 1
+                # Add the final list 
+                retval.append(temp_list)
+
+        return retval
+    
+    # You’ll be given a list where the value at each index represents the size of the group that person belongs to. Your task is to group people into lists of the correct size.
+    def groupBasedOnID(self, group_size_list: list[int]) -> list[list]:
+        retval = []
+        group_size_dict = defaultdict(list)
+
+        index = 0
+        for person in group_size_list:
+            group_size_dict[person].append(index)
+            index = index + 1
+
+        # Sort the dictionary by key size, iterate over them getting the group size and the index of the people of that size
+        for group, list_index in sorted(group_size_dict.items()):
+            # While there are items in the list of the current group
+            while list_index:
+                # Create a new list that has up to the group size number of people in it.
+                iter = 0
+                temp_list = []
+                # Iterate through the current group and add people to a temporary list while removing people from the possible group.
+                while iter < group:
+                    temp_list.append(list_index.pop(0))
+                    iter = iter + 1
+                # Add the final list 
                 retval.append(temp_list)
 
         return retval
