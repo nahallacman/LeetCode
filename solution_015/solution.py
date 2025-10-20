3d65a1e2aba8a731e2389e8475753bd377e95bbd
Cal Barkman <callahan.barkman@gmail.com>
2025-10-19 19:13:47 -0700

    Works better but chokes on giant input.

--- solution_015/solution.py
+++ solution_015/solution.py
@@ -16,6 +16,9 @@ class Solution:
         # Set up return
         retval = []
         nums_len = len(nums)
+        # if(nums_len < 3 or nums_len > 3000):
+        #     return retval
+        j_nums_len = nums_len - 1
         done = False
         i = 0
         j = 1
@@ -30,15 +33,23 @@ class Solution:
                 # Prevent duplicates
                 if( new_list not in retval ):
                     retval.insert(0, new_list)
+            # move k forward one
             k = k + 1
+            # If k has reached the end of the array,
             if(k >= nums_len):
-                i = i + 1
-                j = i + 1
-                k = i + 1
+                # move j forward one
+                j = j + 1
+                # If j has reached 1 away from the end of the array
+                if(j >= j_nums_len):
+                    # move the entire pointer set back to the start of the array, but not losing the index we had for i
+                    i = i + 1
+                    j = i + 1
+                # reset k to be just 1 ahead of j
+                k = j + 1
                 if(k >= nums_len):
                     done = True
 
-        print(f"retval = {retval}")
+        # print(f"retval = {retval}")
         
         return retval
 
